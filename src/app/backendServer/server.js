85c40ec9315868691cecfea471467258e76d0935
xiaoCong2014 <1149023208@qq.com>
2017-09-05 13:03:57 +0800

    去掉 / 首页请求; 让定义尽量写在上面; 用 region 注释

--- src/app/backendServer/server.js
+++ src/app/backendServer/server.js
@@ -3,53 +3,58 @@ var path = require("path");
 var app = express();
 
 app.use(express.static(path.join(__dirname,"/static")));
-app.set("views",path.join(__dirname,"/views"));
+
+// app.set("views",path.join(__dirname,"/views"));
+
 app.set("view engine", "ejs");
 
-app.get('/',function(req,res){
-    // 这里我们使用ejs引擎啦
-    res.render("index");
-    // res.sendFile(__dirname+'/views/index.html');
+/**
+ * 端口号
+ * @type {*|number}
+ */
+var port = process.env.PORT || 8000;
+
+var server = app.listen( port,function(){
+  console.log("listen on port *:" + port );
 });
 
 app.get('/vue',function(request,response){
     response.render('index-vue');
 });
+
 app.get('/onlineusers',function(request,response){
     console.log(io.sockets.adapter.rooms);
     response.send(io.sockets.adapter.rooms);
 });
 
-var port = process.env.PORT || 8000;
-
-var server = app.listen(port,function(){
-    console.log("listen on port *:"+port);
-});
+//浏览器不直接访问这个 , 访问前端服务器
+// app.get('/',function(req,res){
+//   // 这里我们使用ejs引擎啦
+//   res.render("index");
+//   // res.sendFile(__dirname+'/views/index.html');
+// });
 
 // 监听位置好像跟listen() 方法出现位置无关！
 app.get('/test',function(request,response){
     console.log(io.sockets.adapter.rooms);
     response.send('hi test');
 });
+
 /**
  * -------------------------------------------------------- +|
  *             下面集成 socket-io
  * -------------------------------------------------------- +|
  */
-var io = require("socket.io").listen(server);
+var io = require("socket.io").listen( server );
 
-/**
- * + +++++++++++++++++++++++++++++++++++++++++++++++++++ |
- * 
- */
+// region for vue 页面
 
- 
-// for vue 页面
 function  VUEChatHandler(io ,socket){
    this.io = io ;
    this.socket = socket ;
 
 }
+
 VUEChatHandler.prototype.handleConnection = function(){
    // tell all clients that someone connected
     this.io.emit('user joined',this.socket.id) ;
@@ -59,7 +64,7 @@ VUEChatHandler.prototype.handleDisConnecte = function(){
     // tell all clients that someone disconnected
      this.socket.broadcast.emit('user left',this.socket.id) ;
  };
- VUEChatHandler.prototype.handleChatMessage = function(){
+VUEChatHandler.prototype.handleChatMessage = function(){
     // the client sends 'chat.message' event to server
     this.socket.on('chat.message',function(message){
         // emit this event to all clients connected to it
@@ -73,23 +78,19 @@ VUEChatHandler.prototype.handleUserTyping = function(){
        this.io.emit('user typing',username);
     }.bind(this));
 };
-
 VUEChatHandler.prototype.handleStoppedTyping = function(){
     this.socket.on('stopped typing',function(username){
        this.io.emit('stopped typing',username);
     }.bind(this));
 };
-/**
- *              end for vue
- * --------------------------------------   +|
- */
+
+// endregion
 
 var messages = ["Chatroom started"];
 
 io.sockets.on('connection',function(socket){
     // 所有的逻辑将发生在这个代码块
-    console.log("new connection on socket id "+socket.id);
-   
+    console.log("new connection on socket id " + socket.id );
 
     // ------------------------------------- + |
     // 仅对vue页面管用
@@ -101,10 +102,10 @@ io.sockets.on('connection',function(socket){
      // ------------------------------------- + |
 
     // 相对当前socket的 次级全局变量
-    var user ; 
+    var user ;
 
-    // socket 本身 可以作为变量寄宿根  
-   
+    // socket 本身 可以作为变量寄宿根
+
     socket.on("got_new_user",function(data){
         console.log(data) ;
         // var user = data.name;
@@ -142,3 +143,5 @@ io.sockets.on('connection',function(socket){
     });
 });
 
+// region 监听器
+// endregion 监听器
