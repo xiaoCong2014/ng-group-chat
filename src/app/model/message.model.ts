bf9c34f28898920619ea3be551cdb592c593d108
xiaoCong2014 <1149023208@qq.com>
2017-09-05 21:59:16 +0800

    属性 private 改成 public

--- src/app/model/message.model.ts
+++ src/app/model/message.model.ts
@@ -2,9 +2,9 @@
  * 消息 模型
  */
 export class Message {
-  private type: string;
-  private action: string;
-  private user: string;
-  private text: string;
-  private timestamp: string;
+  public type: string;
+  public action: string;// todo 这个字段似乎前后端都没用到 , 要去掉吗
+  public user: string;
+  public text: string;
+  public timestamp: string;
 }
