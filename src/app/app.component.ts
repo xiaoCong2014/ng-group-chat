aa25fb253d96f130599f1d4a466e525e31e81c90
yiqing <yiqing_95@qq.com>
2017-09-05 20:45:01 +0800

    基本功能 完成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { ChatService } from './chat/chat.service';
+
+import * as io from 'socket.io-client';
+
+// import { ChatService } from './chat/chat.service';
 
 export class User {
   id: number;
@@ -12,7 +15,7 @@ export class Message {
   action: string;
   user: string;
   text: string;
-  timestamp: string ;
+  timestamp: string;
 }
 
 
@@ -33,9 +36,10 @@ const USERS: User[] = [
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [ChatService]
+  providers: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
 
   title = 'app';
 
@@ -43,14 +47,103 @@ export class AppComponent {
 
   connectedUsers = [];
   messages = [];
-  message = {
+  message: Message; /* {
     "type": "",
     "action": "",
     "user": "",
     "text": "",
     "timestamp": ""
-  };
+  }; */
+  messageText = '';
   areTyping = [];
 
-  constructor(private chatService: ChatService) { }
+  socket: SocketIOClient.Socket;
+
+
+
+  constructor() {
+    this.socket = io.connect('http://localhost:8000');
+    this.message = new Message();
+    this.message.text = '';
+  }
+
+  ngOnInit() {
+    const socket = this.socket;
+
+    // if server emit 'user joined', update connectedUsers array
+    socket.on('user joined', function (data) {
+      // data is socket-id
+      let socketId = data;
+      /*
+      // get already connected user firstly
+      axios.get('/onlineusers')
+        .then(function (response) {
+          for (var key in response.data) {
+            if (this.connectedUsers.indexOf(key) <= -1) {
+              this.connectedUsers.push(key);
+            }
+          }
+        }.bind(this));
+        */
+
+      let infoMsg = {
+        type: 'info',
+        msg: 'User ' + socketId + ' has joined '
+      };
+      this.messages.push(infoMsg);
+
+
+      this.connectedUsers.push(socketId);
+    }.bind(this));
+
+    // if server emits 'chat.message', update the message array
+    socket.on('chat.message', function (message) {
+      this.messages.push(message);
+    }.bind(this));
+
+    // server emits 'user typing'
+    socket.on('user typing', function (username) {
+      this.areTyping.push(username);
+    }.bind(this));
+
+    // server emits 'stopped typing'
+    socket.on('stopped typing', function (username) {
+
+      let index = this.areTyping.indexOf(username);
+      if (index >= 0) {
+        this.areTyping.splice(index, 1);
+      }
+    }.bind(this));
+
+    // if server broadcasts 'user left', remove leaving user from connectedUsers array
+    socket.on('user left', function (socketId) {
+      let index = this.connectedUsers.indexOf(socketId);
+      // console.log(index);
+      if (index >= 0) {
+        this.connectedUsers.splice(index, 1);
+      }
+      let infoMsg = {
+        type: 'info',
+        'msg': 'User ' + socketId + ' has left '
+      };
+      this.messages.push(infoMsg);
+
+    }.bind(this));
+  }
+
+  send() {
+    let socket = this.socket;
+
+    this.message.type = 'chat';
+    this.message.user = socket.id;
+
+    this.message.timestamp = 'today';
+    // this.message.timestamp = moment().calendar();
+
+    socket.emit('chat.message', this.message);
+    this.message.type = '';
+    this.message.user = '';
+    this.message.text = '';
+    this.message.timestamp = '';
+  }
 }
